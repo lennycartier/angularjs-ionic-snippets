bba3bb640bf4d199f63ff007baa1b3cd73715957
lennycartier <l.cartier@free.fr>
2015-01-04 12:20:04 +0100

    - switch to tabs - set android tabs position to bottom

--- controllers-services/www/js/app.js
+++ controllers-services/www/js/app.js
@@ -17,3 +17,39 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
 })
+
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+	
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+  
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html'
+      }
+    }
+  });
+  
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/dash');
+
+})
+
+.config(function($ionicConfigProvider) {
+	$ionicConfigProvider.platform.android.tabs.position('bottom');
+});
+  
